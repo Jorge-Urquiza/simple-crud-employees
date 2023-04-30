d149c5080ee89599c9895d7d84115fa2afc8a845
Jorge Luis <jorgestudio2017@gmail.com>
2023-04-29 23:05:37 -0400

    feat: simple CRUD employees

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Employee } from './models/employee';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'simple-crud';
+  public employees: Employee[] = [
+    {
+      id: 1,
+      name: 'Juan Carlos',
+      country: 'Bolivia'
+    },
+  ];
+
+  public currentEmployee: Employee = new Employee();
+
+  saveOrUpdate(): void {
+    if (this.currentEmployee.id === 0) {
+      this.currentEmployee.id = this.employees.length + 1;
+      this.employees.push(this.currentEmployee);
+    }
+    this.clean();
+  }
+
+  selectEmployee(employee: Employee) {
+    this.currentEmployee = employee;
+  }
+  delete(): void {
+    if (confirm('Estas seguro que desea eliminar este empleado?')) {
+      this.employees = this.employees.filter(employee => this.currentEmployee.id !== employee.id);
+      this.clean();
+    }
+  }
+
+  clean():void{
+    this.currentEmployee = new Employee();
+  }
+
 }
